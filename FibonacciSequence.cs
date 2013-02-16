3e264a9c991cb73f6439aff058c12f9b9f949903
unknown <diana_gospodinowa@abv.bg>
2013-02-16 09:50:22 +0200

    fibonacci sequence

---
+++ FibonacciSequence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class FibonacciSequence
+    {
+        static void Main()
+        {
+            int i;
+            int n = 100;
+            decimal a = 0;
+            decimal b = 1;
+            decimal c;
+            for (i = 1; i <= n; i++ )
+            {
+                c = a + b;
+                a = b;
+                b = c;                
+                Console.WriteLine(c);
+            }
+        }
+    }
+
